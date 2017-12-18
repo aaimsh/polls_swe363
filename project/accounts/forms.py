883117f7f8330d3f8e14c95106bb3b0ce0a717b9
8amr <c.3amer@gmail.com>
2017-12-18 20:09:41 +0300

    Add files via upload

--- project/accounts/forms.py
+++ project/accounts/forms.py
@@ -2,10 +2,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 
+
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+
+
+
+
 
 
 
