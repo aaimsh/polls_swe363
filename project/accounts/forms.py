3512eaae48542a16af95f8d2d67a29a3c68ab647
aaimsh <aaimsh101@gmail.com>
2017-12-18 20:41:03 +0300

    edit registration form

--- project/accounts/forms.py
+++ project/accounts/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
-
 
 
 class RegisterForm(UserCreationForm):
@@ -14,10 +14,44 @@ class RegisterForm(UserCreationForm):
         for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
 
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise  ValidationError("Username already exists")
+        return username
 
+    def clean_firstname(self):
+        first_name = self.cleaned_data['first_name'].lower()
+        return first_name
 
+    def clean_lastname(self):
+        last_name = self.cleaned_data['last_name'].lower()
+        return last_name
 
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise  ValidationError("Email already exists")
+        return email
 
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
 
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password don't match")
 
+        return password2
 
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        user.first_name = self.clean_firstname()
+        user.last_name = self.clean_lastname()
+        user.save()
+        return user
