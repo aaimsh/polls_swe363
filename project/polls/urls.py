9eebf4992937114963ed236390e35ce917a74757
aaimsh <aaimsh101@gmail.com>
2017-12-27 00:56:49 +0300

    Edit login, profile, pollslist, Add Logout

--- project/polls/urls.py
+++ project/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     url(r'^create-poll/$', views.create_question_view, name='create-poll'),
-    url(r'^$', views.PollsList.as_view(), name='polls-list'),
+    url(r'^', views.PollsList.as_view(), name='polls-list'),
     url(r'^(?P<pk>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 
